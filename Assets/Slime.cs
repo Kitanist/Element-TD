5845db635b52a2d9898f7d19a87b911d509e90c0
Kitanist <eneskerem2516@gmail.com>
2023-08-29 02:02:26 +0300

    slime Go BRRRR

--- Assets/Slime.cs
+++ Assets/Slime.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class Slime : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        yoyoYoYo();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void yoyoYoYo()
     {
-        
+        transform.DOMoveY(2, .5f, false).SetLoops(99,LoopType.Yoyo);
     }
+    
 }
