f00be9bdb5687c93eca91a9a57fbbd9d344020dd
Kitanist <eneskerem2516@gmail.com>
2023-08-29 11:47:22 +0300

    Push

--- Assets/Slime.cs
+++ Assets/Slime.cs
@@ -1,19 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Unity.VisualScripting;
+
 public class Slime : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
-        yoyoYoYo();
-        
+       // yoyoYoYo();
     }
 
     public void yoyoYoYo()
     {
-        transform.DOMoveY(2, .5f, false).SetLoops(99,LoopType.Yoyo);
+        transform.DOMoveY(2, .5f, false).SetLoops(99, LoopType.Restart);
     }
     
 }
